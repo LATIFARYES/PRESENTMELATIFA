571ee23bd4dcc2fbca8c60b693d6b57bea531475
LATIFARYES <latifaryes@gmail.com>
2023-10-11 17:26:56 +0300

    Initial Commit

---
+++ PRESENTMELATIFA/PRESENTMELATIFAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PRESENTMELATIFAApp.swift
+//  PRESENTMELATIFA
+//
+//  Created by LATIFA on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PRESENTMELATIFAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
