4a14e00e0b012200cd5c8b5645dadb7f566a850c
LATIFARYES <latifaryes@gmail.com>
2023-10-11 17:30:25 +0300

    I ADD A NEW LNE

--- PRESENTMELATIFA/ContentView.swift
+++ PRESENTMELATIFA/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text(" HELLO WORLD !")
+                    .bold()
+                    .foregroundStyle(.blue)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                
+
+               
+                Image("LDEV")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                    .clipShape(Circle())
+                
+                    .padding(4)
+                
+                    .background(Circle().strokeBorder(Color.purple, lineWidth: 10 ))
+                
+                VStack(alignment: .leading) {
+                Text(" LATIFA ").bold()
+                .font(.largeTitle
+                )
+                .foregroundStyle(.black)
+                Text("أقوى لطوف بالدنيا ")
+                        .bold()
+                    Text(" ♡👩🏻‍💻💸🪐🔭🕯️")
+
+                }
     }
 }
 
